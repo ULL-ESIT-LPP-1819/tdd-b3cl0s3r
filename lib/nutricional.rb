55ec285e949ed9c17280521deff7b9c761cfc063
b3cl0s3r <b3cl0s3r@localhost.localdomain>
2018-11-05 16:13:06 +0000

    Creado almacenamiento informacion obligatoria

--- lib/nutricional.rb
+++ lib/nutricional.rb
@@ -1,5 +1,16 @@
 require "nutricional/version"
 
-module Nutricional
-  # Your code goes here...
+class Tag
+  attr_reader :nombre, :venergetico, :cgrasas, :cgsaturadas, :hidratos, :azucares, :proteinas, :sal
+
+  def initialize(v1, v2, v3, v4, v5, v6, v7, v8)
+    @nombre = v1
+    @venergetico = v2
+    @cgrasas = v3
+    @cgsaturadas = v4
+    @hidratos = v5
+    @azucares = v6
+    @proteinas = v7
+    @sal = v8
+  end
 end
--- spec/nutricional_spec.rb
+++ spec/nutricional_spec.rb
@@ -1,9 +1,38 @@
 RSpec.describe Nutricional do
-  it "has a version number" do
-    expect(Nutricional::VERSION).not_to be nil
+
+  before :each do
+    @tag = Tag.new("Freeway", 10, 0, 0, 2.1, 2.1, 0, 0.02)
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "Nombre etiqueta correcto" do
+    expect(@tag.nombre).to eq("Freeway")
+  end
+
+  it "Valor energético correcto" do
+    expect(@tag.venergetico).to eq(10)
+  end
+
+  it "Valor cantidad de grasas correcto" do
+    expect(@tag.cgrasas).to eq(0)
+  end
+
+  it "Valor cantidad de grasas saturadas correcto" do
+    expect(@tag.cgsaturadas).to eq(0)
+  end
+
+  it "Valor hidratos de carbono correcto" do
+    expect(@tag.hidratos).to eq(2.1)
+  end
+
+  it "Valor azúcares correcto" do
+    expect(@tag.azucares).to eq(2.1)
+  end
+
+  it "Proteinas" do
+    expect(@tag.proteinas).to eq(0)
+  end
+
+  it "Sal" do
+    expect(@tag.sal).to eq(0.02)
   end
 end
