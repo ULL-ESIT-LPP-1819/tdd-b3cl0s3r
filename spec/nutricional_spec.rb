f76e79037212ccc7f620bb1885870b63b6e885b4
b3cl0s3r <b3cl0s3r@localhost.localdomain>
2018-11-05 11:52:05 +0000

    Primeras pruebas etiqueta

--- spec/nutricional_spec.rb
+++ spec/nutricional_spec.rb
@@ -1,9 +1,39 @@
 RSpec.describe Nutricional do
-  it "has a version number" do
-    expect(Nutricional::VERSION).not_to be nil
+
+  before :each do
+    @tag = Point.new("Freeway", 10, 0, 0, 2.1, 2.1, 0, 0.02)
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "Nombre etiqueta correcto" do
+    expect(@p1.nombre).to eq("Freeway")
+  end
+
+  it "Valor energético correcto" do
+    expect(@p1.nombre).to eq(10)
+  end
+
+  it "Valor cantidad de grasas correcto" do
+    expect(@p1.nombre).to eq(0)
+  end
+
+  it "Valor cantidad de grasas saturadas correcto" do
+    expect(@p1.nombre).to eq(0)
+  end
+
+  it "Valor hidratos de carbono correcto" do
+    expect(@p1.nombre).to eq(2.1)
+  end
+
+  it "Valor azúcares correcto" do
+    expect(@p1.nombre).to eq("Freeway")
+  end
+
+  it "Proteinas" do
+    expect(@p1.nombre).to eq("Freeway")
+  end
+
+  it "Sal" do
+    expect(@p1.nombre).to eq("Freeway")
   end
 end
+
