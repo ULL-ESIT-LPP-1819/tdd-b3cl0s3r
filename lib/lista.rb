288e30e88e576f7bea04f5c64a1338bd54664d58
b3cl0s3r <b3cl0s3r@localhost.localdomain>
2018-11-13 10:45:18 +0000

    Pequeños cambios

---
+++ lib/lista.rb
@@ -0,0 +1,25 @@
+require 'nutricional'
+
+class Lista
+  attr_accessor :head, :tail
+
+  def initialize()
+  end
+
+  def insert(nodo)
+    # Cojo el tail, set next el nodo, nuevo nodo = tail
+  end
+
+  def pop
+    # Coges el tail, accedes a la anterior etiqueta, asigno esa etiqueta al tail
+  end
+
+  def shift
+    # Coges el head, accedes a la siguiente etiqueta, asigno esa etiqueta al head
+  end
+
+  def recorrer
+    # recorrer
+  end
+  
+end
