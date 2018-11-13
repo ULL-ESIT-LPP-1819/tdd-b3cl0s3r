251b54b6a985e3c4cdd4484ff878ceefec1f1181
b3cl0s3r <b3cl0s3r@localhost.localdomain>
2018-11-13 16:45:55 +0000

    Pruebas añadidas

--- spec/lista_spec.rb
+++ spec/lista_spec.rb
@@ -1,27 +1,19 @@
-RSpec.describe Lista do
+RSpec.describe Nutricional do
 
   before :each do
-    etiqueta1 = Tag.new("Freeway", 10, 0, 0, 2.1, 2.1, 0, 0.02)
-    etiqueta2 = Tag.new("Hamburguesa", 500, 63, 0, 2.1, 2.1, 0, 13)
-    etiqueta3 = Tag.new("Pescado", 250, 0, 23, 2.1, 2.1, 0, 19)
-    etiqueta4 = Tag.new("Manzana", 99, 6, 0, 2.1, 2.1, 0, 1)
-    etiqueta5 = Tag.new("Pera", 143, 13, 0, 2.1, 2.1, 0, 3)
-    nodo1 = Node.new(etiqueta1, nil, nil)
-    nodo2 = Node.new(etiqueta1, nil, nil)
-    nodo3 = Node.new(etiqueta1, nil, nil)
-    nodo4 = Node.new(etiqueta1, nil, nil)
-    nodo5 = Node.new(etiqueta1, nil, nil)
-    lista = Pene.new()
+    @etiqueta1 = Tag.new("Freeway", 10, 0, 0, 2.1, 2.1, 0, 0.02)
+    @etiqueta2 = Tag.new("Hamburguesa", 500, 63, 0, 2.1, 2.1, 0, 13)
+    @etiqueta3 = Tag.new("Pescado", 250, 0, 23, 2.1, 2.1, 0, 19)
+    @etiqueta4 = Tag.new("Manzana", 99, 6, 0, 2.1, 2.1, 0, 1)
+    @etiqueta5 = Tag.new("Pera", 143, 13, 0, 2.1, 2.1, 0, 3)
+    @lista = Lista.new()
   end
 
-  it "Push correcto" do
-    expect(@tag.nombre).to eq("Freeway")
-
-    lista.push(nodo1)
-    lista.push(nodo2)
-    lista.push(nodo3)
-    lista.push(nodo4)
-    lista.push(nodo5)
+  it "Insert correcto" do
+    expect(@lista.insert(@etiqueta1)).equal?(Node)
+    expect(@lista.insert(@etiqueta2)).equal?(Node)
+    expect(@lista.insert(@etiqueta3)).equal?(Node)
+    expect(@lista.insert(@etiqueta4)).equal?(Node)
+    expect(@lista.insert(@etiqueta5)).equal?(Node)
   end
-
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require "bundler/setup"
 require "nutricional"
+require "lista"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
