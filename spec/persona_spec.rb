2af2b376cc8a090e85c61fea613c7dc5a0df1ea9
b3cl0s3r <b3cl0s3r@localhost.localdomain>
2018-11-19 17:39:45 +0000

    añadidas algunas pruebas

---
+++ spec/persona_spec.rb
@@ -0,0 +1,26 @@
+RSpec.describe Nutricional do
+
+  before :each do
+    @tag = Persona.new()
+  end
+
+  it "Comprobación clase de un objeto" do
+##    expect(@p1).to be_a_kind_of(Point::Point)
+##    expect(@p1).to be_a_kind_of(Object) 
+  end
+
+
+  it "Comprobación tipo de un objeto" do
+
+  end
+
+
+  it "Comprobación pertenece a una jerarquía" do
+
+  end
+
+
+  it "Pruebas para clasificar según masa corporal" do
+
+  end
+end
